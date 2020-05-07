58f550f5666615f921009bb5195eedb20dc4cdba
pranaliandre <pranaliandre06@gmail.com>
2020-05-07 13:32:29 -0500

    Added: UC4: addition two lengths in inches

--- QuantityMeasurementTest/QuantityMeasurementTest.cs
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -292,5 +292,45 @@ namespace QuantityMeasurementTest
             double fiveCentimeter = 5.0;
             Assert.AreEqual(twoInch, fiveCentimeter);
         }
+        /// <summary>
+        /// Test case 1.25 Given 2 Inch addition to 2 Inch Equal to 4 inch
+        /// </summary>
+        [Test]
+        public void GivenTwoLengthsInInch_ForAddition_ReturnInInch()
+        {
+            double twoInch = compareLength.AddTwoLenghtsInInch(Length.Unit.INCH, 2, Length.Unit.INCH, 2);
+            double addTwoInch = 4.0;
+            Assert.AreEqual(twoInch, addTwoInch);
+        }
+        /// <summary>
+        /// Test case 1.26 Given 1 Feet addition to 2 Inch Equal to 14 inch
+        /// </summary>
+        public void GivenTwoLengthsOneInFeetAndSecondInInch_ShouldReturnAdditionInInch()
+        {
+            double oneFeetOneInch = compareLength.AddTwoLenghtsInInch(Length.Unit.FEET, 1, Length.Unit.INCH, 2);
+            double addTwoInch = 14.0;
+            Assert.AreEqual(addTwoInch, oneFeetOneInch);
+        }
+        /// <summary>
+        /// Test case 1.27 Given 1 Feet addition to 1 Feet Equal to 24 inch
+        /// </summary>
+        [Test]
+        public void GivenTwoLengthsInFeet_ReturnAdditionInInch()
+        {
+            double addTwoFeet = compareLength.AddTwoLenghtsInInch(Length.Unit.FEET, 1, Length.Unit.FEET, 1);
+            double addTwoInch = 24.0;
+            Assert.AreEqual(addTwoInch, addTwoFeet);
+        }
+        /// <summary>
+        /// Test case 1.28 Given 1 inch addition to 1 Feet Equal to 24 inch
+        /// </summary>
+        [Test]
+        public void GivenTwoLengthsOneInInchAndOneInCentimeter_ReturnAdditionInInch()
+        {
+            double oneInchOneCentimeter = compareLength.AddTwoLenghtsInInch(Length.Unit.INCH, 2, Length.Unit.CENTIMETER, 2.5);
+            double addTwoInch = 3.0;
+            Assert.AreEqual(addTwoInch, oneInchOneCentimeter);
+        }
+
     }
 }
