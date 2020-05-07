70326b0000f659649ecc916785a7fa74f6e6192e
pranaliandre <pranaliandre06@gmail.com>
2020-05-07 00:35:55 -0500

    Added: Given 0 feet and 0 feet should return equal

--- QuantityMeasurementProblem/QuantityMeasurement.cs
+++ QuantityMeasurementProblem/QuantityMeasurement.cs
@@ -1,10 +1,51 @@
 ﻿using System;
-
 namespace QuantityMeasurementProblem
 {
-    class QuantityMeasurement
+    public class QuantityMeasurement
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Variable
+        /// </summary>
+        public double value;
+        /// <summary>
+        /// Method for getting inch
+        /// </summary>
+        /// <param name=value"></param>
+        /// <returns></returns>
+        public double GetFeet(double value)
+        {
+            return value*12;
+        }
+        /// <summary>
+        /// Override Equals Method.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            //null check
+            if (obj == null)
+            {
+                return false;
+            }
+            //Reference check
+            if (obj == this)
+            {
+                return true;
+            }
+            //Equality check
+            return (this.value == ((QuantityMeasurement)obj).value);
+        }
+        /// <summary>
+        /// Overriding GetHashCode Method.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    
+    static void Main(string[] args)
         {
             Console.WriteLine("!!!Welcome to Quantity Measurement");
         }
---
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using QuantityMeasurementProblem;
+using System;
+
+namespace QuantityMeasurementTest
+{
+    public class Tests
+    {
+        readonly QuantityMeasurement qualityMeasurement = new QuantityMeasurement();
+        [SetUp]
+        public void Setup()
+        {
+        }
+        /// <summary>
+        /// Test Case 1.1 Given 0 Feet And 0 Feet Should Return Equal.
+        /// </summary>
+        [Test]
+        public void Given0FeetAnd0Feet_ShouldReturnEqual()
+        {
+            //Creating conversion instance
+            double value1 = qualityMeasurement.GetFeet(0.0);
+            double value2 = qualityMeasurement.GetFeet(0.0);
+            Assert.AreEqual(value1, value2, 0.0);
+        }
+    }
+}
